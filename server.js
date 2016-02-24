586724fc29e31fb38799081fee32d63556f61beb
David Stichter <david.stichter3414@gmail.com>
2016-02-24 18:53:29 -0500

    Added postgres for heroku

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ var PORT = process.env.NODE_ENV || 8000;
 //Bcrypt
 var bcrypt = require('bcryptjs')
 //Sequelize
-var sequelize = require('sequelize');
-//!!!! NEED TO ADD DATABASE AND USER TO USE SEQUELIZE
-
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('postgres://postgres:password@localhost/rutgersflyer');
+//postgres
+var pg = require('pg');
 //Handlebars
 var expressHandlebars = require('express-handlebars');
 app.engine('handlebars', expressHandlebars({
@@ -24,3 +25,34 @@ var passport = require('passport');
 var passportLocal = require('passport-local');
 app.use(passport.initialize());
 app.use(passport.session());
+
+var User = sequelize.define('User', {
+  firstname: Sequelize.STRING,
+  lastname: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+	password: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			len: {
+				args: [8,32],
+				msg: "Your password must be between 8-32 characters"
+			},
+		}
+	}
+});
+
+
+User.findAll({firstname: 'David'}).then(function(results){
+  console.log(results)
+})
+
+sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+      console.log("Listening on:" + PORT)
+  });
+});
