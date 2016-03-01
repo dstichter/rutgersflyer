a45fdac8044d77f547d4abf2cc11fc827970a720
David Stichter <david.stichter3414@gmail.com>
2016-02-29 20:54:38 -0500

    Saving changes

--- public/js/events.js
+++ public/js/events.js
@@ -8,5 +8,23 @@ $(document).ready(function() {
     $('#modal1').openModal()
   })
 
+  $('.starHover').mouseenter( function(){
+    var holdThis = $(this)
+    $('.starHover').each(function(){
+      if(parseInt($(this).attr('data-star')) <= parseInt($(holdThis).attr('data-star'))){
+        $(this).attr('src', '../images/star_full.png')
+      }
+    })
+
+  } ).mouseleave( function(){
+    $('.starHover').each(function(){
+      if(parseInt($('.starHover').attr('data-star')) <= parseInt($(this).attr('data-star'))){
+        $(this).attr('src', '../images/star_empty.png')
+      }
+    })
+
+  }).click(function(){
+    $('.starHover')
+  })
 
 });
